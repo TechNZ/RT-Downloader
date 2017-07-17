afa80c12fe364f30904d0ae249ed5350eab4be91
TechNZ <techie@technz.info>
2017-07-17 22:19:30 +1200

    update 17/07/2017
    fixed parsing of episode URL's

--- series.sh
+++ series.sh
@@ -6,9 +6,9 @@ episodes=${page_source#*tab-content-episodes}
 episodes=${episodes%grid-blocks*}
 episodes=`grep href <<< "$episodes"`
 for i in ${episodes}; do
-	episode=${i#href=\"}
-	episode=${episode%\"*}
-	if [ ${episode} == '<a' ]; then continue; fi 
-	./download.sh ${episode}
+        episode=${i#href=\"}
+        episode=${episode%\"*}
+        if [[ $episode != *\/episode\/* ]]; then continue; fi
+        ./download.sh ${episode}
 done
 exit 0
